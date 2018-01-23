73c2b067f24fe95e199f7395c0905276258159ed
Ivan Ryapolov <34157311+rilite@users.noreply.github.com>
2018-01-23 10:36:27 +0200

    Update script.js

--- 01/script.js
+++ 01/script.js
@@ -2,25 +2,25 @@ var car1 = {
 	model:'Славута',
 	disk: 11,
 	run: function(){
-		console.log('модель автомобиля ' + car1.model + ' размер диска ' + car1.disk + ' дюймов');
+	console.log('Модель автомобиля ' + car1.model + ' Размер диска ' + car1.disk + ' дюймов');
 	}
-}
+};
 
 var car2 = {
 	model:'Таврия',
 	disk: 13,
 	run: function(){
-		console.log('модель автомобиля ' + car2.model + ' размер диска ' + car2.disk + ' дюймов');
+	console.log('Модель автомобиля ' + car2.model + ' Размер диска ' + car2.disk + ' дюймов');
 	}
-}
+};
 
 var car3 = {
 	model:'Ланос',
 	disk: 15,
 	run: function(){
-		console.log('модель автомобиля ' + car3.model + ' размер диска ' + car3.disk + ' дюймов');
+	console.log('Модель автомобиля ' + car3.model + ' Размер диска ' + car3.disk + ' дюймов');
 	}
-}
+};
 car1.run();
 car2.run();
 car3.run();
