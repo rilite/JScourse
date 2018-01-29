50bff1e45165671dad039ccfd64c55ad085d7a90
Ivan Ryapolov <34157311+rilite@users.noreply.github.com>
2018-01-29 17:52:11 +0200

    Update script.js

--- 05/script.js
+++ 05/script.js
@@ -5,7 +5,7 @@ var fibonacci = [0, 1];
 for (i = 2; i < n; i ++) {
   fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
 }
-console.log(fibonacci); // slice отрезает от массива первые n элементов, если n < 2
+console.log(fibonacci);
 
 
 
@@ -15,16 +15,16 @@ let left = 0;
 let timer;			
 
 function autoSlider() {									
-	timer = setTimeout(function(){       
-		let polosa = document.getElementById('polosa'); 
-		left -= 128; 
-		if (left < -512) { 
-			left = 0;				
-			clearTimeout (timer);  
-	 	}
-	 	polosa.style.left = left + 'px'; // для #polosa применяется стиль позиционирования слева в пикселах
-	 	autoSlider();
-	}, 1000); 
+    timer = setTimeout(function(){       
+        let polosa = document.getElementById('polosa'); 
+        left -= 128; 
+        if (left < -512) { 
+            left = 0;				
+            clearTimeout (timer);  
+        }
+ 	polosa.style.left = left + 'px'; // для #polosa применяется стиль позиционирования слева в пикселах
+ 	autoSlider();
+    }, 1000); 
 }
 autoSlider();
 
