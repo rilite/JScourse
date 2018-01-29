9324ebde92e1fc47339a50420591fd4d0618fba6
Ivan Ryapolov <34157311+rilite@users.noreply.github.com>
2018-01-29 17:19:50 +0200

    Update script.js

--- 03/script.js
+++ 03/script.js
@@ -3,21 +3,21 @@
 // cycle with precondition
 var i = 1;
 while (i <= 5) {
-	console.log(i);
-	i++;
+    console.log(i);
+    i++;
 }
 
 // cycle with postcondition
 var i = 1;
 do {
-	console.log(i);
-	i++;
+    console.log(i);
+    i++;
 }
 while(i <= 5);
 
 // cycle
 for (let i = 1; i <= 5; i++) {
-	console.log(i);
+    console.log(i);
 }
 
 
@@ -28,21 +28,21 @@ for (let i = 1; i <= 5; i++) {
 // cycle with precondition
 var i = 5;
 while(i >= 1){
-	console.log(i);
-	i--;
+    console.log(i);
+    i--;
 }
 
 // cycle with precondition
 var i = 5;
 do {
-	console.log(i);
-	i--;
+    console.log(i);
+    i--;
 }
 while(i >= 1);
 
 // cycle
 for (let i = 5; i >= 1; i--) {
-	console.log(i);
+    console.log(i);
 }
 
 
@@ -53,19 +53,19 @@ for (let i = 5; i >= 1; i--) {
 // cycle with precondition
 var i = 1;
 while(i <= 10){
-	console.log('3 * '+ i + ' = '+  3*i);
-	i++;
+    console.log('3 * '+ i + ' = '+  3*i);
+    i++;
 }
 
 // cycle with postcondition
 var i = 1;
 do {
-	console.log('3 * '+ i + ' = '+  3*i);
-	i++;
+    console.log('3 * '+ i + ' = '+  3*i);
+    i++;
 }
 while(i <= 10);
 
 // cycle
 for (let i = 1; i <= 10; i++) {
-	console.log('3 * '+ i + ' = '+  3*i);
+    console.log('3 * '+ i + ' = '+  3*i);
 }
