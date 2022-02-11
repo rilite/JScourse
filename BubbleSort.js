33c11b243406f3d34a02c48c8bdede9bb8c030f7
Ivan Ryapolov <34157311+rilite@users.noreply.github.com>
2022-02-11 10:03:09 +0200

    Update BubbleSort.js

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,9 +1,9 @@
-var arr = [5, 50, 23, 1, 70, 0, 10, 15, 99, 30];
+const arr = [5, 50, 23, 1, 70, 0, 10, 15, 99, 30];
 console.log(arr);
-for (var i = 1; i < arr.length- 1 ; i++) {
-    for (var j = 1 ; j < arr.length - 1; j++){
+for (let i = 1; i < arr.length; i++) {
+    for (let j = 1 ; j < arr.length; j++){
         if (arr[j] < arr[j - 1]) {
-            var temp = arr[j];
+            let temp = arr[j];
             arr[j] = arr[j-1];
             arr[j - 1] = temp;
         }
